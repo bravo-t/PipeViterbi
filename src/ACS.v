1fa7b800e1ccc4f624354181a83a9a2598b6b737
bravo-t <13inback@gmail.com>
2014-06-02 17:02:09 +0800

    Added pointers function (MRE)

--- src/ACS.v
+++ src/ACS.v
@@ -1,44 +1,86 @@
 module ACS(self_state,
+		   input_sig,
 		   data_recv,
+		   addr_in_1,
+		   addr_in_2,
 		   PMin1,
 		   PMin2,
-		   PMout);
+		   PMout,
+		   addr_out,
+		   data_rdy,
+		   dec_out);
 
-input [1:0] self_state,data_recv;
-input [6:0] PMin1,PMin2;
-output [6:0] PMout;
+	input input_sig;
+	input [1:0] self_state,data_recv,addr_in_1,addr_in_2;
+	input [6:0] PMin1,PMin2;
+	output data_rdy,dec_out;
+	output [1:0] addr_out;
+	output [6:0] PMout;
 
-wire [6:0] PM_cal_1,PM_cal_2;
-wire [1:0] ham_dist_1,ham_dist_2;
-reg [1:0] path_id_1,path_id_2;
+	reg [6:0] PM_cal_1,PM_cal_2;
+	reg [6:0] PMout;
+	reg [1:0] ham_dist_1,ham_dist_2;
+	reg [1:0] path_id_1,path_id_2;
+	reg [1:0] addr_out;
+	reg dec_out;
 
-always @(*)
-begin
-	case(self_state)
-	2'b00:begin
-		path_id_1=2'b00;
-		path_id_2=2'b11;
+
+	always @(*)
+	begin
+		if (input_sig) begin
+			case(self_state)
+				2'b00:begin
+					path_id_1=2'b00;
+					path_id_2=2'b11;
+					dec_out=0;
+				end
+				2'b01:begin
+					path_id_1=2'b10;
+					path_id_2=2'b01;
+					dec_out=0;
+				end
+				2'b10:begin
+					path_id_1=2'b11;
+					path_id_2=2'b00;
+					dec_out=1;
+				end
+				2'b11:begin
+					path_id_1=2'b01;
+					path_id_2=2'b10;
+					dec_out=1;
+				end
+			endcase
+			data_rdy=1;
+			PM_cal_1 = PMin1+ham_dist_1;
+			PM_cal_2 = PMin2+ham_dist_2;
+			if(PM_cal_1<PM_cal_2) begin
+				PMout = PM_cal_1;
+				addr_out = addr_in_1;
+			end
+			else if(PM_cal_1>PM_cal_2) begin
+				PMout = PM_cal_2;
+				addr_out = addr_in_2;
+			end
+			else begin
+				if(ham_dist_1<ham_dist_2) begin
+					PMout = PM_cal_1;
+					addr_out = addr_in_1;
+				end
+				else begin
+					PMout = PM_cal_2;
+					addr_out = addr_in_2;
+				end
+			end
+		end
+		else begin 		//no input signals, so we don't care what the circuit is doing
+			path_id_1=2'bx;
+			path_id_2=2'bx;
+			data_rdy=0;
+			addr_out=2'bxx;
+		end
 	end
-	2'b01:begin
-		path_id_1=2'b10;
-		path_id_2=2'b01;
-	end
-	2'b10:begin
-		path_id_1=2'b11;
-		path_id_2=2'b00;
-	end
-	2'b11:begin
-		path_id_1=2'b01;
-		path_id_2=2'b10;
-	end
-	endcase
-end
 
-ham_compute u_ham_com_1(.data_recv(data_recv),.path_id(path_id_1),.ham_dist(ham_dist_1));
-ham_compute u_ham_com_2(.data_recv(data_recv),.path_id(path_id_2),.ham_dist(ham_dist_2));
-
-assign PM_cal_1 = PMin1+ham_dist_1;
-assign PM_cal_2 = PMin2+ham_dist_2;
-assign PMout = (PM_cal_1>PM_cal_2)?PM_cal_2:PM_cal_1;
+	ham_compute u_ham_com_1(.data_recv(data_recv),.path_id(path_id_1),.ham_dist(ham_dist_1));
+	ham_compute u_ham_com_2(.data_recv(data_recv),.path_id(path_id_2),.ham_dist(ham_dist_2));
 
 endmodule
--- src/BM_mem.v
+++ src/BM_mem.v
@@ -2,25 +2,40 @@ module PM_mem(data_id,
 			  PM_clk,
 			  PM_rst,
 		      PM_in,
-		      pointer_in,
+		      data_en,
+		      addr_in,
+		      dec_in,
 		      PM_out,
-		      pointer_out,
+		      addr_out,
+		      data_rdy,
+		      dec_out,
 		      data_id_out);
-	input PM_clk,PM_rst;
+	input PM_clk,PM_rst,dec_in,data_en;
+	input [1:0] addr_in;
 	input [3:0] data_id;
 	input [6:0] PM_in;
+	output dec_out,data_rdy;
+	output [1:0] addr_out;
 	output [3:0] data_id_out;
 	output [6:0] PM_out;
+	reg dec_out,data_rdy;
+	reg [1:0] addr_out;
 	reg [3:0] data_id_out;
 	reg [6:0] PM_out;
 
 	always @(posedge PM_clk or negedge PM_rst) begin
 		if (!PM_rst) begin
+			dec_out <= 1'b0;
+			data_rdy <= 1'b0;
+			addr_out <= 2'b00;
 			PM_out <= 7'b0000000;
 			data_id_out <= 4'b0000;
 		end
 		else
 		begin
+			dec_out <= dec_in;
+			data_rdy <= data_en;
+			addr_out <= addr_in;
 			PM_out <=PM_in;
 			data_id_out <= data_id;
 		end
