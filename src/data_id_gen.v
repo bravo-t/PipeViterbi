d25b821fe6fe8ee45ebcf0bdc380269493823e87
bravo-t <13inback@gmail.com>
2014-06-18 18:32:29 +0800

    Fix data_id width

--- src/data_id_gen.v
+++ src/data_id_gen.v
@@ -4,18 +4,15 @@ module data_id_gen(clk,
 
 	input clk;
 	input rst;
-	output [3:0] data_id;
-	reg [3:0] data_id;
+	output [2:0] data_id;
+	reg [2:0] data_id;
 
 	always @(posedge clk or negedge rst) begin
 		if (!rst) 
-			data_id <= 4'b0000;
+			data_id <= 3'b000;
 		else
 		begin
-			if (data_id < 4'b1000)
-				data_id <= data_id + 1'b1;
-			else
-				data_id <= 4'b0000;
+			data_id <= data_id + 1'b1;
 		end
 	end
 
