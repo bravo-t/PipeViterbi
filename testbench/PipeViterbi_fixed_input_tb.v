6befbc88ab76ffae6b7d801d5a2bab889c1b9931
bravo-t <13inback@gmail.com>
2014-07-05 18:30:15 +0800

    Revert "A big bug and the case is still open"
    This reverts commit cbeb98a0d848402d6c4bb933f64fc973aada9f51.

---
+++ testbench/PipeViterbi_fixed_input_tb.v
@@ -0,0 +1,26 @@
+module test();
+
+	reg clk,rst;
+	wire [7:0] data_dec;
+
+	initial
+	begin
+		$dumpfile("FixedInput.vcd");
+		$dumpvars;
+		rst=0;
+		#5 rst=1;
+		#100 $finish;
+	end
+
+	initial begin
+		clk = 0;
+		forever #1 clk = ~clk;
+	end
+
+PipeViterbi u_PipeViterbi(.clk(clk),
+				   .rst(rst),
+				   //.input_valid(1'b1);
+				   .data_recv(16'b1101100100111100),
+				   .data_dec(data_dec));
+
+endmodule
